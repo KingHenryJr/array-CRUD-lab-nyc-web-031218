3feda977a0eeb5da0d1c42c7f1d3638897f718c2
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-23 17:32:48 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  var array = ["hello","My","name","is"]
+  array = ["hello","My","name","is"]
 end
 
 def add_element_to_end_of_array(array, element)
